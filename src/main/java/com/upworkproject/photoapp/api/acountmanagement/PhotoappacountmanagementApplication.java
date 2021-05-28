19f54faab2418f230ce4dd04379ad63440b8f071
s.pouya <s.pouya@pinket.ir>
2021-05-28 17:40:50 +0430

    first commit

---
+++ src/main/java/com/upworkproject/photoapp/api/acountmanagement/PhotoappacountmanagementApplication.java
@@ -0,0 +1,13 @@
+package com.upworkproject.photoapp.api.acountmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoappacountmanagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotoappacountmanagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/upworkproject/photoapp/api/acountmanagement/PhotoappacountmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upworkproject.photoapp.api.acountmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoappacountmanagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
